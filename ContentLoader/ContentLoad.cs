ef2792da633313d6849a09df3cc69c7c2ca61248
Abel <abel.o.d@outlook.com>
2019-04-01 23:06:50 -0300

    - first commit

---
+++ ContentLoader/ContentLoad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContentLoader
+{
+    public class ContentLoad : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public ContentLoad()
+        {
+            Visibility = Visibility.Collapsed;
+        }
+
+        Visibility visibility;
+        public Visibility Visibility
+        {
+            get { return visibility; }
+            set { visibility = value; NotifyPropertyChanged("Visibility"); }
+        }
+
+        public void LoadContent()
+        {
+            Visibility = Visibility.Visible;
+
+            var t = new Thread(() =>
+            {
+                Thread.Sleep(3000);
+                Visibility = Visibility.Collapsed;
+            });
+
+            t.Start();
+        }
+    }
+}
---
+++ ContentLoader/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContentLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ContentLoad contentLoad;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contentLoad = new ContentLoad();
+
+            this.DataContext = contentLoad;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            contentLoad.LoadContent();
+        }
+    }
+}
